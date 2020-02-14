ee129c9969bcdb692a0ec91f8b3e94b614e824b7
Patrick Mutamba <mutambamilambop@johndeere.com>
2020-02-14 12:06:19 -0600

    Hangman iteration 02142020 midday

---
+++ scriptsHangman.js
@@ -0,0 +1,8 @@
+console.log('Welcome to your first project')
+
+var guessWordArr = [] 
+var keyboardArr = [['a','b','c','d','e','f','g'],
+               ['h','i','j','k','l','m','n'],
+               ['o','p','q','r','s','t','u'],
+               ['v','w','x','y','z']]
+
