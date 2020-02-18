6bd372d84154a2bf96c23da755df142ac6910535
Patrick Mutamba <mutambamilambop@johndeere.com>
2020-02-18 12:44:55 -0600

    Hangman iteration 02182020 midday

--- scriptsHangman.js
+++ scriptsHangman.js
@@ -11,13 +11,11 @@ var keyboardArr = [['a','b','c','d','e','f','g'],
                ['v','w','x','y','z']]
 const keyboardTotalKeys = 26
 
-let kRowsOne = document.querySelector('.keyRowsOne')
-let kRowsTwo = document.querySelector('.keyRowsTwo')
-let kRowsThree = document.querySelector('.keyRowsThree')
-let kRowsFour = document.querySelector('.keyRowsFour')
-console.log(kRowsOne)
+const pressStart = document.querySelector('#startBtn')
+const guessLetterIn = document.querySelector('.guessWordIn')
 
-var currentWorld = ""
+
+var currentWord = ""
 var numberOfguess = 0
 var numberOfGguessRemaining = 0
 
@@ -28,14 +26,14 @@ buildKeyboard()
 
 function buildKeyboard() {
     keyboardArr.forEach(function(element,index,kbArr) {
-        console.log(element)
+//        console.log(element)
         const arrKBLen = element.length
         letterArray = element;
-        console.log(arrKBLen)
+//        console.log(arrKBLen)
         for (let i = 0; i < arrKBLen; i++) {
             const appendKeyNode = document.querySelector(`.keyRows${index+1}`)
-            console.log(`.keyRows${index+1}`)
-            console.log(appendKeyNode)
+//            console.log(`.keyRows${index+1}`)
+//            console.log(appendKeyNode)
             var divNodeKB = document.createElement("DIV")
             divNodeKB.setAttribute("class", "gameKeys")
             divNodeKB.setAttribute("id", letterArray[i])
@@ -43,41 +41,108 @@ function buildKeyboard() {
             appendKeyNode.append(divNodeKB)
         }
     })
-    const pressStart = document.querySelector('#startBtn')
+    // const pressStart = document.querySelector('#startBtn')
     pressStart.addEventListener("click", onPressStart)
 //    pressStart.removeEventListener("click", onPressStart)
 }
 
 function onPressStart() {
+
+    generateRandomWord()
     // divNodeKB.addEventListener("click", onPressLetter)
     // Activate keyboard keys 
-    console.log('I am in the OnPressStart button')
-    console.log(this)
+//    console.log('I am in the OnPressStart button')
+//    console.log(this)
     const activateKeysArray = document.querySelectorAll('.gameKeys')
-    console.log(activateKeysArray)
+//    console.log(activateKeysArray)
     activateKeysArray.forEach(elem => {
         elem.addEventListener('click', activateKeysFunction)
         elem.style.color = 'rgb(0,0,0)'
         elem.style.border = '2px solid rgb(0, 0, 0)' 
     })
     //Activate reset button
-    const pressPlayAgain = document.querySelector('#resetBtn')
-    pressPlayAgain.addEventListener("click", () => {
-        console.log('Enabling reset button')
-    })    
+    activatePlayAgain()
+//   console.log(`Setting up style for input screen ${guessLetterIn}`)
+    guessLetterIn.style.border = '2px solid rgb(0, 0, 0'
+    guessLetterIn.style.background = 'rgb(255, 255, 255'
+   
+    pressStart.style.background = 'rgb(249, 250, 245)'
+    pressStart.style.color = 'rgb(183, 163, 163)'
+    pressStart.style.border = '2px solid rgb(183, 163, 163)' 
+
     this.removeEventListener("click", onPressStart)
 }
 
 function activateKeysFunction(event) {
     console.log(this)
-    let elKey = `#${event.target.id}`
+    let elKey = `${event.target.id}`
     console.log(`This is ${elKey}`)
-    console.log("Press start working, activating key board")
+    console.log(currentWord)
+    let currWord = currentWord.toLowerCase()
+    let compareWordArr = currWord.split(''); 
+    console.log(`This first value ${this.textContent}`)
+    compareWordArr.forEach(function(letterCompare, indexCompare, compareArray){
+        console.log(`This.value ${letterCompare}`)
+        if (letterCompare === elKey) {
+            guessWordArr.push(letterCompare)
+            console.log(`#${letterCompare}${indexCompare}`)
+            console.log(document.querySelector(`#${letterCompare}${indexCompare}`))
+            document.querySelector(`#${letterCompare}${indexCompare}`).innerHTML = letterCompare
+        } else {
+            guessWordArr.push(' ')
+        }
+        console.log(`Value of ${elKey}`)
+    })
+    console.log(`The new compare array ${guessWordArr}`)
+    
+//    console.log("Press start working, activating key board")
     this.style.color = 'rgb(183, 163, 163)'
     this.style.border = '2px solid rgb(183, 163, 163)' 
+
     this.removeEventListener('click',activateKeysFunction)
     }
 
+function activatePlayAgain(event) {
+//    console.log('Iam in play again' + this)
+    const pressPlayAgain = document.querySelector('#resetBtn')
+    pressPlayAgain.style.color = 'rgb(0, 0, 0)' 
+    pressPlayAgain.style.background = 'rgb(192, 192, 192'
+    pressPlayAgain.style.border = '2px solid rgb(0, 0, 0)'  
+    pressPlayAgain.addEventListener("click", () => {
+//        console.log('Enabling reset button')
+        window.location.reload(true)
+        // buildKeyboard()
+        // console.log('Disabling reset button')
+        // this.removeEventListener('click',activatePlayAgain)
+    })
+}
+
+function generateRandomWord() {
+    const indexRdArr = Math.floor(Math.random() * randomWordArray.length)
+    console.log(`The randowm array index is ${indexRdArr}`)
+    currentWord = randomWordArray[indexRdArr]
+    console.log('the ramdom word is ' + currentWord)
+    let currentWordArr = currentWord.toLowerCase().split('');
+    console.log(currentWordArr)
+    const currentWordArrLen = currentWordArr.length
+    currentWordArr.forEach(function(currElem, currIndex, currWarr){
+        var currNodeLetter = document.createElement("DIV")
+        currNodeLetter.setAttribute("class", "currWordLetter")
+        currNodeLetter.setAttribute("id", `${currElem}${currIndex}`)
+        if (currElem === ' ') {
+            console.log(currElem)
+            currNodeLetter.innerHTML = ' '
+        } else {
+            currNodeLetter.innerHTML = '_'
+        }
+        guessLetterIn.append(currNodeLetter)       
+    })
+
+} 
+
+function compareInput() {
+
+}
 // function onPressLetter() {
 //     compareLetterToGuessWord()
 //     setUpFontRemoveListener()
