32041630a9dae18560580e531bb959762879fc94
Patrick Mutamba <mutambamilambop@johndeere.com>
2020-02-19 11:01:21 -0600

    Hangman iteration 02192020 midday

--- scriptsHangman.js
+++ scriptsHangman.js
@@ -3,32 +3,58 @@ console.log('Welcome to your first project')
 var randomWordArray = ['Dubuque', 'Maquoketa', 'Davenport', 'Des Moines', 'Bettendorf',
                     'Iowa City', 'Cedar Rapids', 'Waterloo', 'Van Horne', 'Grimes',
                     'Johnston', 'Ankeny', 'Ames', 'Urbandale', 'Sioux City', 
-                    'Cedar Falls', 'Asburry', 'Ottumwa', 'Cedar Falls', 'Mason City']
-var guessWordArr = [] 
+                    'Cedar Falls', 'Asburry', 'Ottumwa', 'Cedar Falls', 'Mason City',
+                    'Adel', 'Adair', 'Clive', 'Ely', 'Nashua']
+
 var keyboardArr = [['a','b','c','d','e','f','g'],
                ['h','i','j','k','l','m','n'],
                ['o','p','q','r','s','t','u'],
                ['v','w','x','y','z']]
+
+var scaffoldCanvas = [[' ', '|', '_', '_', '_', '_', ' '],
+                       [' ', '|', ' ', ' ', ' ', ' ', ' '],
+                       [' ', '|', ' ', ' ', ' ', ' ', ' '],
+                       [' ', '|', ' ', ' ', ' ', ' ', ' '],
+                       [' ', '|', ' ', ' ', ' ', ' ', ' '],
+                       [' ', '|', ' ', ' ', ' ', ' ', ' '],
+                       [' ', '|', ' ', ' ', ' ', ' ', ' '],
+                       ['_', '|', '_', '_', '_', '_', '_']]
+
+var guessWordArr = [] 
+var compareWordArr = []
+
 const keyboardTotalKeys = 26
 
 const pressStart = document.querySelector('#startBtn')
 const guessLetterIn = document.querySelector('.guessWordIn')
+const onStartGameDisplay = document.querySelector('#gameDisplay')
 
+console.log(onStartGameDisplay)
 
 var currentWord = ""
-var numberOfguess = 0
+var numberOfGuess = 0
+var numberOfMissedGuess = 0
 var numberOfGguessRemaining = 0
+var numberOfLives = 8
+var compareMatch = false
+var gameWon = false
 
-/* Load game key board upon starting the game
-*
+main()
+
+function main() {
+    buildKeyboard()
+    buildScafold()
+    pressStart.addEventListener("click", onPressStart)
+}
+
+/* 
+*   Load game key board upon starting the game 
 */
-buildKeyboard()
-
 function buildKeyboard() {
     keyboardArr.forEach(function(element,index,kbArr) {
 //        console.log(element)
         const arrKBLen = element.length
-        letterArray = element;
+        letterArray = element
 //        console.log(arrKBLen)
         for (let i = 0; i < arrKBLen; i++) {
             const appendKeyNode = document.querySelector(`.keyRows${index+1}`)
@@ -36,17 +62,35 @@ function buildKeyboard() {
 //            console.log(appendKeyNode)
             var divNodeKB = document.createElement("DIV")
             divNodeKB.setAttribute("class", "gameKeys")
-            divNodeKB.setAttribute("id", letterArray[i])
+            divNodeKB.setAttribute("id", `keys${letterArray[i]}`)
             divNodeKB.innerHTML = letterArray[i]
             appendKeyNode.append(divNodeKB)
         }
     })
-    // const pressStart = document.querySelector('#startBtn')
-    pressStart.addEventListener("click", onPressStart)
-//    pressStart.removeEventListener("click", onPressStart)
+}
+
+/*
+*   Build initial gallows mat upon starting the game
+*/
+function buildScafold() {
+    scaffoldCanvas.forEach(function(scElement, scIndex, scArr) {
+        const arrSCLen = scElement.length
+        scaffoldArray = scElement
+        for (let j = 0; j < arrSCLen; j++) {
+            const appendSCNode = document.querySelector(`.HangLayer${scIndex+1}`)
+            var divNodeSC = document.createElement("DIV")
+            divNodeSC.setAttribute("class", "scafoldPiece")
+            divNodeSC.setAttribute("id", `scaffold${scIndex}${j}`)
+            divNodeSC.innerHTML = scaffoldArray[j]
+            appendSCNode.append(divNodeSC)            
+
+        }
+    })
 }
 
 function onPressStart() {
+    const onStartHangingArea = document.querySelectorAll('.scafoldPiece')
+    console.log(onStartHangingArea)
 
     generateRandomWord()
     // divNodeKB.addEventListener("click", onPressLetter)
@@ -56,50 +100,97 @@ function onPressStart() {
     const activateKeysArray = document.querySelectorAll('.gameKeys')
 //    console.log(activateKeysArray)
     activateKeysArray.forEach(elem => {
-        elem.addEventListener('click', activateKeysFunction)
+        elem.addEventListener('click', playTheGameFunction)
         elem.style.color = 'rgb(0,0,0)'
         elem.style.border = '2px solid rgb(0, 0, 0)' 
     })
     //Activate reset button
     activatePlayAgain()
 //   console.log(`Setting up style for input screen ${guessLetterIn}`)
-    guessLetterIn.style.border = '2px solid rgb(0, 0, 0'
-    guessLetterIn.style.background = 'rgb(255, 255, 255'
+    guessLetterIn.style.border = '2px solid rgb(0, 0, 0)'
+    guessLetterIn.style.background = 'rgb(255, 255, 255)'
    
     pressStart.style.background = 'rgb(249, 250, 245)'
     pressStart.style.color = 'rgb(183, 163, 163)'
     pressStart.style.border = '2px solid rgb(183, 163, 163)' 
+    
+
+    onStartHangingArea.forEach(function(hangElem, hangIndex, hangArr) {
+        hangElem.style.color = 'rgb(0, 0, 0)'
+    })
+
+    onStartGameDisplay.style.border = '2px solid rgb(0, 0, 0)'
+    onStartGameDisplay.style.color = 'rgb(0, 0, 0)'
+
+    alert("Please guess the name of a city in the state of iowa.");
 
     this.removeEventListener("click", onPressStart)
 }
 
-function activateKeysFunction(event) {
+function playTheGameFunction(event) {
     console.log(this)
-    let elKey = `${event.target.id}`
+//    let elKey = `${event.target.id}`
+    let elKey = `${this.textContent}`
     console.log(`This is ${elKey}`)
     console.log(currentWord)
-    let currWord = currentWord.toLowerCase()
-    let compareWordArr = currWord.split(''); 
+    // let currWord = currentWord.toLowerCase()
+    let compareWordArr = currentWord.toLowerCase().split(''); 
+    console.log('Array in play game', compareWordArr)
     console.log(`This first value ${this.textContent}`)
     compareWordArr.forEach(function(letterCompare, indexCompare, compareArray){
         console.log(`This.value ${letterCompare}`)
+        if (indexCompare === 0) {
+            numberOfGuess -= 1
+        }
         if (letterCompare === elKey) {
             guessWordArr.push(letterCompare)
+            console.log(`The guess array building ${guessWordArr}`)
+            console.log(guessWordArr)
             console.log(`#${letterCompare}${indexCompare}`)
             console.log(document.querySelector(`#${letterCompare}${indexCompare}`))
             document.querySelector(`#${letterCompare}${indexCompare}`).innerHTML = letterCompare
-        } else {
-            guessWordArr.push(' ')
+            evaluateGameResult()
+            compareMatch = true
+            } 
+        if ((indexCompare + 1) === currentWord.length) {
+            console.log('indexCompare ' + indexCompare)
+            if (numberOfGuess !== 0) {      
+                if (compareMatch === false) {
+                    console.log('compareMatch f ' + compareMatch )
+                    numberOfMissedGuess += 1
+                    console.log('numberOfMissedGuess' + numberOfMissedGuess)
+                    numberOfLives -= 1
+                    console.log('numberOfLives' + numberOfLives)
+                    updateScaffold()
+ 
+                } else {
+                    console.log('compareMatch t ' + compareMatch )
+                    compareMatch = false
+                }
+                if (gameWon === false) {
+                    if (numberOfLives === 0) {
+                        document.querySelector('#gameDisplay').innerHTML = `Game over! You are out of lives. The guess is ${currentWord}`
+                        
+                    } else {
+                        document.querySelector('#gameDisplay').innerHTML = `You have ${numberOfGuess} guesses and ${numberOfLives} lives remaining.`
+                    }
+                }               
+            } else {
+                document.querySelector('#gameDisplay').innerHTML = `Game over! You are out of guesses. The guess is ${currentWord}`
+            }
         }
+
+        } )
+
         console.log(`Value of ${elKey}`)
-    })
     console.log(`The new compare array ${guessWordArr}`)
+ //   evaluateGameResult()
     
 //    console.log("Press start working, activating key board")
     this.style.color = 'rgb(183, 163, 163)'
     this.style.border = '2px solid rgb(183, 163, 163)' 
 
-    this.removeEventListener('click',activateKeysFunction)
+    this.removeEventListener('click',playTheGameFunction)
     }
 
 function activatePlayAgain(event) {
@@ -137,12 +228,62 @@ function generateRandomWord() {
         }
         guessLetterIn.append(currNodeLetter)       
     })
-
+    if (currentWordArrLen <= 5) {
+        numberOfGuess = 8
+    } else {
+        numberOfGuess = currentWordArrLen + 3
+    }
+    console.log(`number of guess random ${numberOfGuess}`)
 } 
 
-function compareInput() {
+function evaluateGameResult() {
+    console.log(guessWordArr) 
+    console.log(compareWordArr)
+    var removeBlank = []
+    let ctrial = currentWord.toLowerCase().split(''); 
+    console.log('ctrial',ctrial)
+    ctrial.forEach(function(remElement, remIndex, remArr){
+        if (remElement !== ' ') {
+            removeBlank.push(remArr[remIndex]) 
+        }
+    })
+//    removeBlank = ctrial.filter( (ctrialEl) => { ctrialEl !== ' '})
+    console.log('removeBlamk', removeBlank)
+    console.log(guessWordArr.length)
+    console.log(removeBlank.length)
+    if (guessWordArr.length == removeBlank.length) {
+        gameOverWin()
+    }
 
 }
+
+function gameOverWin() {
+    console.log('GameOverWin')
+    document.querySelector('#gameDisplay').innerHTML = 'Congratulations! You have won the game.'
+    gameWon = true
+}
+
+function updateScaffold() {
+    console.log('update scaffold', document.querySelector('#scaffold15'))
+    if (numberOfMissedGuess === 1) {
+        document.querySelector('#scaffold15').innerHTML = '|'
+    } else if (numberOfMissedGuess === 2) {
+        document.querySelector('#scaffold25').innerHTML = 'O'
+    } else if (numberOfMissedGuess === 3) {
+        document.querySelector('#scaffold35').innerHTML = '|'
+    } else if (numberOfMissedGuess === 4) {
+        document.querySelector('#scaffold34').innerHTML = '/'
+    } else if (numberOfMissedGuess === 5) {
+        document.querySelector('#scaffold36').innerHTML = '\\'
+    } else if (numberOfMissedGuess === 6) {
+        document.querySelector('#scaffold45').innerHTML = '|'
+    } else if (numberOfMissedGuess === 7) {
+        document.querySelector('#scaffold54').innerHTML = '/'
+    } else if (numberOfMissedGuess === 8) {
+        document.querySelector('#scaffold56').innerHTML = '\\'
+    } 
+}
+
 // function onPressLetter() {
 //     compareLetterToGuessWord()
 //     setUpFontRemoveListener()
